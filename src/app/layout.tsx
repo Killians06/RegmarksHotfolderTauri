dbafaa6e89958eb608856f244e5f6209d5de5ea5
Onivoid <Skullyfox.yt@gmail.com>
2024-08-13 22:37:53 +0200

    Refactoring + Add : Add color picker + cut Layout to multiple components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/custom/ThemeProvider";
 import { Suspense } from "react";
-import { WebviewWindow } from "@tauri-apps/api/window";
-
-import Link from "next/link";
-
-import {
-    Home,
-    Package,
-    Settings,
-    ShoppingCart,
-    Users,
-    X,
-    Minus,
-    Maximize2,
-    Minimize2,
-} from "lucide-react";
-
-import { DarkModeSelector } from "@/components/custom/DarkModeSelector";
-
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog";
-
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from "@/components/ui/tooltip";
-
-import { CustomMenu } from "@/components/custom/CustomMenu";
-import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-
 import { cn } from "@/lib/utils";
+import { Sidebar } from "@/components/custom/Sidebar";
+import { ControlMenu } from "@/components/custom/ControlMenu";
+import { Inter as FontSans } from "next/font/google";
+import { ThemeProvider } from "@/components/custom/ThemeProvider";
 
 const fontSans = FontSans({
     subsets: ["latin"],
@@ -55,20 +18,6 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const [fullWidth, setFullWidth] = useState(false);
-    const [appWindow, setAppWindow] = useState<WebviewWindow>();
-
-    async function setupAppWindow() {
-        const appWindow = (await import("@tauri-apps/api/window")).appWindow;
-        setAppWindow(appWindow);
-    }
-
-    const minimize = async () => await appWindow?.minimize();
-    const close = async () => await appWindow?.close();
-
-    useEffect(() => {
-        setupAppWindow();
-    }, []);
 
     return (
         <html lang="en" suppressHydrationWarning className="static min-w-full">
@@ -81,207 +30,11 @@ export default function RootLayout({
             >
                 <ThemeProvider attribute="class" defaultTheme="dark">
                     <Suspense fallback={<p>Loading ... </p>}>
-                        <div className="absolute right-4 top-4 flex gap-3">
-                            <Button
-                                variant="ghost"
-                                size="menuButton"
-                                onClick={minimize}
-                            >
-                                <Minus />
-                            </Button>
-                            <Button
-                                variant="ghost"
-                                size="menuButton"
-                                onClick={close}
-                            >
-                                <X />
-                            </Button>
-                        </div>
+
+                        <ControlMenu />
 
                         <div className="max-h-screen w-full flex rounded-l-3xl overflow-hidden">
-                            <div
-                                className={`border-r bg-card relative transition-size duration-150 ${
-                                    fullWidth ? "w-[280px]" : "w-[100px]"
-                                }`}
-                            >
-                                <Button
-                                    size={fullWidth ? "icon" : "iconSm"}
-                                    onClick={() => setFullWidth(!fullWidth)}
-                                    className="absolute right-0 top-[50%] translate-y-[-50%] translate-x-1/2"
-                                >
-                                    {fullWidth ? (
-                                        <Minimize2 />
-                                    ) : (
-                                        <Maximize2 className="h-4 w-4" />
-                                    )}
-                                </Button>
-                                <div className="flex h-full max-h-screen flex-col ">
-                                    <CustomMenu fullWidth={fullWidth} />
-                                    <Separator />
-                                    <div className="flex flex-col justify-between h-full pb-4">
-                                        <nav
-                                            className={`${
-                                                !fullWidth && "gap-3"
-                                            } grid items-start text-sm font-medium px-4 pt-3`}
-                                        >
-                                            <Link
-                                                href="/"
-                                                className={`${
-                                                    !fullWidth &&
-                                                    "justify-center"
-                                                } flex items-center gap-3 rounded-lg py-2 text-muted-foreground transition-all hover:text-primary`}
-                                            >
-                                                <TooltipProvider
-                                                    delayDuration={10}
-                                                >
-                                                    <Tooltip>
-                                                        <TooltipTrigger>
-                                                            <Home className="h-4 w-4" />
-                                                        </TooltipTrigger>
-                                                        <TooltipContent side="right">
-                                                            <p>Homepage</p>
-                                                        </TooltipContent>
-                                                    </Tooltip>
-                                                </TooltipProvider>
-                                                {fullWidth && "Homepage"}
-                                            </Link>
-                                            <Link
-                                                href="/page1"
-                                                className={`${
-                                                    !fullWidth &&
-                                                    "justify-center"
-                                                } flex items-center gap-3 rounded-lg py-2 text-muted-foreground transition-all hover:text-primary`}
-                                            >
-                                                <TooltipProvider
-                                                    delayDuration={50}
-                                                >
-                                                    <Tooltip>
-                                                        <TooltipTrigger>
-                                                            <ShoppingCart className="h-4 w-4" />
-                                                        </TooltipTrigger>
-                                                        <TooltipContent side="right">
-                                                            <p>Page 1</p>
-                                                        </TooltipContent>
-                                                    </Tooltip>
-                                                </TooltipProvider>
-                                                {fullWidth && "Page 1"}
-                                            </Link>
-                                            <Link
-                                                href="/page2"
-                                                className={`${
-                                                    !fullWidth &&
-                                                    "justify-center"
-                                                } flex items-center gap-3 rounded-lg py-2 text-muted-foreground transition-all hover:text-primary`}
-                                            >
-                                                <TooltipProvider
-                                                    delayDuration={50}
-                                                >
-                                                    <Tooltip>
-                                                        <TooltipTrigger>
-                                                            <Package className="h-4 w-4" />
-                                                        </TooltipTrigger>
-                                                        <TooltipContent side="right">
-                                                            <p>Page 2</p>
-                                                        </TooltipContent>
-                                                    </Tooltip>
-                                                </TooltipProvider>
-                                                {fullWidth && "Page 2"}
-                                            </Link>
-                                            <Link
-                                                href="/page3"
-                                                className={`${
-                                                    !fullWidth &&
-                                                    "justify-center"
-                                                } flex items-center gap-3 rounded-lg py-2 text-muted-foreground transition-all hover:text-primary`}
-                                            >
-                                                <TooltipProvider
-                                                    delayDuration={50}
-                                                >
-                                                    <Tooltip>
-                                                        <TooltipTrigger>
-                                                            <Users className="h-4 w-4" />
-                                                        </TooltipTrigger>
-                                                        <TooltipContent side="right">
-                                                            <p>Page 3</p>
-                                                        </TooltipContent>
-                                                    </Tooltip>
-                                                </TooltipProvider>
-                                                {fullWidth && "Page 3"}
-                                            </Link>
-                                        </nav>
-                                        <div
-                                            className={`flex ${
-                                                fullWidth
-                                                    ? "justify-between"
-                                                    : "justify-center"
-                                            } items-center px-4`}
-                                        >
-                                            {fullWidth && (
-                                                <p className="text-xs text-muted-foreground text-nowrap">
-                                                    <span className="text-primary">
-                                                        Software Name
-                                                    </span>{" "}
-                                                    - by Onivoid
-                                                </p>
-                                            )}
-                                            <Dialog>
-                                                <DialogTrigger className="flex items-center gap-3 rounded-lg py-2 text-muted-foreground transition-all hover:text-primary">
-                                                    <TooltipProvider
-                                                        delayDuration={50}
-                                                    >
-                                                        <Tooltip>
-                                                            <TooltipTrigger
-                                                                asChild
-                                                            >
-                                                                <Settings className="h-4 w-4" />
-                                                            </TooltipTrigger>
-                                                            <TooltipContent side="right">
-                                                                <p>Settings</p>
-                                                            </TooltipContent>
-                                                        </Tooltip>
-                                                    </TooltipProvider>
-                                                </DialogTrigger>
-                                                <DialogContent>
-                                                    <DialogHeader>
-                                                        <DialogTitle>
-                                                            Settings
-                                                        </DialogTitle>
-                                                        <DialogDescription
-                                                            asChild
-                                                        >
-                                                            <div>
-                                                                <ul>
-                                                                    <li className="flex items-center gap-5 text-foreground">
-                                                                        DarkMode
-                                                                        :{" "}
-                                                                        <DarkModeSelector />
-                                                                    </li>
-                                                                </ul>
-                                                            </div>
-                                                        </DialogDescription>
-                                                    </DialogHeader>
-                                                </DialogContent>
-                                            </Dialog>
-                                        </div>
-                                    </div>
-                                    {/* <div className="mt-auto p-4">
-                        <Card x-chunk="dashboard-02-chunk-0">
-                            <CardHeader className="p-2 pt-0 md:p-4">
-                                <CardTitle>Upgrade to Pro</CardTitle>
-                                <CardDescription>
-                                    Unlock all features and get unlimited access
-                                    to our support team.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                                <Button size="sm" className="w-full">
-                                    Upgrade
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    </div> */}
-                                </div>
-                            </div>
+                            <Sidebar />
                             <main className="flex flex-col rounded-r-3xl overflow-hidden w-full">
                                 {children}
                             </main>
---
+++ src/components/custom/ColorPicker.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { cn } from "@/lib/utils";
+import { Paintbrush } from "lucide-react";
+import { useMemo, useState, useEffect } from "react";
+import { hexToHSL } from "@/utils/hexToHsl";
+
+export function PickerExample() {
+    const [background, setBackground] = useState("#3B3B98");
+
+    useEffect(() => {
+        document.documentElement.style.setProperty('--primary', hexToHSL(background));
+    }, [background]);
+
+    return (
+        <GradientPicker
+                background={background}
+                setBackground={setBackground}
+            />
+    );
+}
+
+export function GradientPicker({
+    background,
+    setBackground,
+    className,
+}: {
+    background: string;
+    setBackground: (background: string) => void;
+    className?: string;
+}) {
+    const solids = [
+        "#3B3B98",
+        "#B33771",
+        "#FC427B",
+        "#FEA47F",
+    ];
+
+    const defaultTab = useMemo(() => {
+        return "solid";
+    }, [background]);
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button
+                    variant={"outline"}
+                    className={cn(
+                        "w-[220px] justify-start text-left font-normal",
+                        !background && "text-muted-foreground",
+                        className
+                    )}
+                >
+                    <div className="w-full flex items-center gap-2">
+                        {background ? (
+                            <div
+                                className="h-4 w-4 rounded !bg-center !bg-cover transition-all"
+                                style={{ background }}
+                            ></div>
+                        ) : (
+                            <Paintbrush className="h-4 w-4" />
+                        )}
+                        <div className="truncate flex-1">
+                            {background ? background : "Pick a color"}
+                        </div>
+                    </div>
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-64">
+                <Tabs defaultValue={defaultTab} className="w-full">
+                    <TabsContent
+                        value="solid"
+                        className="flex flex-wrap gap-1 mt-2"
+                    >
+                        {solids.map((s) => (
+                            <div
+                                key={s}
+                                style={{ background: `hsl(${hexToHSL(s)})` }}
+                                className="rounded-md h-6 w-6 cursor-pointer active:scale-105"
+                                onClick={() => setBackground(s)}
+                            />
+                        ))}
+                    </TabsContent>
+                </Tabs>
+
+                <Input
+                    id="custom"
+                    value={background}
+                    className="col-span-2 h-8 mt-4"
+                    onChange={(e) => setBackground(e.currentTarget.value)}
+                />
+            </PopoverContent>
+        </Popover>
+    );
+}
+
+const GradientButton = ({
+    background,
+    children,
+}: {
+    background: string;
+    children: React.ReactNode;
+}) => {
+    return (
+        <div
+            className="p-0.5 rounded-md relative !bg-cover !bg-center transition-all"
+            style={{ background }}
+        >
+            <div className="bg-popover/80 rounded-md p-1 text-xs text-center">
+                {children}
+            </div>
+        </div>
+    );
+};
