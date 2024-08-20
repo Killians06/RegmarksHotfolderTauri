63f6bfa7f8e6740f4254804f0262f80da327d694
Onivoid <Skullyfox.yt@gmail.com>
2024-08-20 10:56:26 +0200

    Bugfix : DraggableRegion breaking the display

--- src/components/custom/CustomMenu.tsx
+++ src/components/custom/CustomMenu.tsx
@@ -6,14 +6,14 @@ import { DraggableRegion } from "@/components/custom/DraggableRegion";
 export const CustomMenu = ({ fullWidth }: { fullWidth: boolean }) => {
     return (
         <>
-            <div
-                className={`${
-                    fullWidth
-                        ? "grid grid-cols-4 px-4 py-6"
-                        : "flex justify-center items-center p-5"
-                } text-foreground`}
-            >
-                <DraggableRegion>
+            <DraggableRegion>
+                <div
+                    className={`${
+                        fullWidth
+                            ? "grid grid-cols-4 px-4 py-6"
+                            : "flex justify-center items-center p-5"
+                    } text-foreground`}
+                >
                     {fullWidth && (
                         <div className="flex items-center justify-center col-span-3 overflow-hidden">
                             <h1 className="font-bold uppercase text-primary text-nowrap">
@@ -24,8 +24,8 @@ export const CustomMenu = ({ fullWidth }: { fullWidth: boolean }) => {
                     <div className="col-span-1 flex items-center justify-center">
                         <CircleUser className="h-6 w-6" />
                     </div>
-                </DraggableRegion>
-            </div>
+                </div>
+            </DraggableRegion>
         </>
     );
 };
